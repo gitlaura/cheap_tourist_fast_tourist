5cb8f5ddd89710069b95a9b1cc69bec2cf50983c
Laura Rokita <laura.rokita@gmail.com>
2014-07-15 22:29:36 -0500

    Create repo.

---
+++ start.rb
@@ -0,0 +1,7 @@
+require_relative 'lib/runner.rb'
+
+flights_to_parse = "sample-input.txt"
+departure_city = "A"
+arrival_city = "Z"
+
+Runner.run_flight_route_calculator(flights_to_parse,departure_city,arrival_city)
