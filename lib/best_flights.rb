81659089dd68abc7c08c862387ef5db28e20bfb0
Laura Rokita <laura.rokita@gmail.com>
2014-07-29 12:51:04 -0500

    update best flights to use enumerable

--- lib/best_flights.rb
+++ lib/best_flights.rb
@@ -1,30 +1,26 @@
 class BestFlights
 	class << self
 		def get_cheapest_flight(all_flight_paths)
-			lowest_price = nil
-			all_flight_paths.each do |flight|
-				if lowest_price.nil? || flight.price < lowest_price
-					@best_flight = flight
-					lowest_price = flight.price
-				elsif lowest_price == flight.price
-					@best_flight = get_fastest_flight([@best_flight, flight])
-				end
+			cheapest_flight = all_flight_paths.min_by {|path| path.price}
+			cheapest_flights = all_flight_paths.select {|path| path.price == cheapest_flight.price}
+			if cheapest_flights.size > 1
+				best_flight = get_fastest_flight(cheapest_flights)
+			else
+				best_flight = cheapest_flights[0]
 			end
-			@best_flight
+			best_flight
 		end
 
 		def get_fastest_flight(all_flight_paths)
-			shortest_length = nil
-			all_flight_paths.each do |flight|
-				length = flight.arr - flight.dep
-				if shortest_length.nil? || length < shortest_length
-					@best_flight = flight
-					shortest_length = length
-				elsif shortest_length == length
-					@best_flight = get_cheapest_flight([@best_flight, flight])
-				end
+			shortest_flight = all_flight_paths.min_by {|path| path.arr - path.dep}
+			shortest_length = shortest_flight.arr - shortest_flight.dep
+			shortest_flights = all_flight_paths.select {|path| (path.arr - path.dep) == shortest_length}
+			if shortest_flights.size > 1
+				best_flight = get_cheapest_flight(shortest_flights)
+			else
+				best_flight = shortest_flights[0]
 			end
-			@best_flight
+			best_flight
 		end
 	end
 end
