a333a2eaa66f840aef0bc5bcfb84699684bf7a9a
Laura Rokita <laura.rokita@gmail.com>
2014-07-29 12:59:26 -0500

    update best_flights

--- lib/best_flights.rb
+++ lib/best_flights.rb
@@ -1,24 +1,24 @@
 class BestFlights
 	class << self
 		def get_cheapest_flight(all_flight_paths)
-			cheapest_flight = all_flight_paths.min_by {|path| path.price}
-			cheapest_flights = all_flight_paths.select {|path| path.price == cheapest_flight.price}
-			if cheapest_flights.size > 1
-				best_flight = get_fastest_flight(cheapest_flights)
+			cheapest_path = all_flight_paths.min_by {|path| path.price}
+			cheapest_paths = all_flight_paths.select {|path| path.price == cheapest_path.price}
+			if cheapest_paths.size > 1
+				best_flight = get_fastest_flight(cheapest_paths)
 			else
-				best_flight = cheapest_flights[0]
+				best_flight = cheapest_paths[0]
 			end
 			best_flight
 		end
 
 		def get_fastest_flight(all_flight_paths)
-			shortest_flight = all_flight_paths.min_by {|path| path.arr - path.dep}
-			shortest_length = shortest_flight.arr - shortest_flight.dep
-			shortest_flights = all_flight_paths.select {|path| (path.arr - path.dep) == shortest_length}
-			if shortest_flights.size > 1
-				best_flight = get_cheapest_flight(shortest_flights)
+			shortest_path = all_flight_paths.min_by {|path| path.arr - path.dep}
+			shortest_length = shortest_path.arr - shortest_path.dep
+			shortest_paths = all_flight_paths.select {|path| (path.arr - path.dep) == shortest_length}
+			if shortest_paths.size > 1
+				best_flight = get_cheapest_flight(shortest_paths)
 			else
-				best_flight = shortest_flights[0]
+				best_flight = shortest_paths[0]
 			end
 			best_flight
 		end
